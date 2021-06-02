4532839a2a0b6873f8ee32922595a2ab7950918a
TitoCodes <yourtitocodes@gmail.com>
2021-06-02 22:07:14 +0800

    Added resume page.

---
+++ frontend/src/pages/Resume.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <section class="hero is-fluid is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="is-size-2">
+            Resume Part
+            <strong>Coming soon!</strong>
+          </div>
+          <br />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+const aboutDescription = 'My Resume';
+const imagePath = 'http://www.titocodes.com/img/main-logo.687a6f89.png';
+
+export default {
+  name: 'Resume',
+  metaInfo: function () {
+    return {
+      title: `About Page - ${this.$website_name}`,
+      link: [{ rel: 'canonical', href: this.$website_address }],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: aboutDescription },
+        { property: 'og:description', content: aboutDescription },
+        { property: 'og:title', content: `About Page- ${this.$website_name}` },
+        {
+          property: 'og:site_name',
+          content: `About Page- ${this.$website_name}`,
+        },
+        { property: 'og:type', content: 'website' },
+        {
+          property: 'og:url',
+          content: this.$website_address,
+        },
+        {
+          property: 'og:image',
+          content: imagePath,
+        },
+        { property: 'og:description', content: aboutDescription },
+        { name: 'twitter:card', content: 'summary' },
+        {
+          name: 'twitter:site',
+          content: this.$website_address,
+        },
+        { name: 'twitter:title', content: `About Page- ${this.$website_name}` },
+        {
+          name: 'twitter:description',
+          content: aboutDescription,
+        },
+        { itemprop: 'name', content: `About Page- ${this.$website_name}` },
+        { itemprop: 'description', content: aboutDescription },
+        {
+          itemprop: 'image',
+          content: imagePath,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+@import '../scss/global.scss';
+
+h1,
+h2,
+h3,
+h6,
+strong,
+.subtitle,
+.container {
+  color: $green !important;
+}
+li {
+  color: $orange;
+}
+</style>
